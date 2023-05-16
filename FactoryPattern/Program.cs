25cb9d127ec1628a034c8819c698e4f391e8996b
AlissaStegall <alissaistegall@gmail.com>
2023-05-16 15:16:53 -0700

    factory pattern exercise complete

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace FactoryPattern
+﻿using System.Buffers.Text;
+using System.ComponentModel;
+
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the number of tires for the vehicle you want to create");
+
+            int tireCount;
+            var input = int.TryParse(Console.ReadLine(), out tireCount);
+
+            var vehicle = VehicleFactory.GetVehicle(tireCount);
+            
+            vehicle.Drive();
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace FactoryPattern
+{
+	public static class VehicleFactory
+	{
+		public static IVehicle GetVehicle(int tireCount)
+		{
+			switch (tireCount)
+			{
+				case 2:
+					return new Motorcycle();
+					break;
+				case 4:
+					return new Car();
+					break;
+				default:
+					return new Car();
+					break;
+			}
+		}
+
+	}
+}
+
